ddded897f249a06ad980b8f8322a600da0b2f80b
mak <makindosx@gmail.com>
2020-04-30 01:33:58 +0300

    Add files via upload

--- acc_register/index.php
+++ acc_register/index.php
@@ -84,6 +84,32 @@ background-color:grey;
 </style>
 
 
+<style type="text/css">
+<!--
+a.gflag {vertical-align:middle;font-size:32px;padding:1px 0;background-repeat:no-repeat;background-image:url(//gtranslate.net/flags/32.png);}
+a.gflag img {border:0;}
+a.gflag:hover {background-image:url(//gtranslate.net/flags/32a.png);}
+#goog-gt-tt {display:none !important;}
+.goog-te-banner-frame {display:none !important;}
+.goog-te-menu-value:hover {text-decoration:none !important;}
+body {top:0 !important;}
+#google_translate_element2 {display:none!important;}
+-->
+</style>
+
+<div id="google_translate_element2"></div>
+<script type="text/javascript">
+function googleTranslateElementInit2() {new google.translate.TranslateElement({pageLanguage: 'el',autoDisplay: false}, 'google_translate_element2');}
+</script><script type="text/javascript" src="https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit2"></script>
+
+
+<script type="text/javascript">
+/* <![CDATA[ */
+eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('6 7(a,b){n{4(2.9){3 c=2.9("o");c.p(b,f,f);a.q(c)}g{3 c=2.r();a.s(\'t\'+b,c)}}u(e){}}6 h(a){4(a.8)a=a.8;4(a==\'\')v;3 b=a.w(\'|\')[1];3 c;3 d=2.x(\'y\');z(3 i=0;i<d.5;i++)4(d[i].A==\'B-C-D\')c=d[i];4(2.j(\'k\')==E||2.j(\'k\').l.5==0||c.5==0||c.l.5==0){F(6(){h(a)},G)}g{c.8=b;7(c,\'m\');7(c,\'m\')}}',43,43,'||document|var|if|length|function|GTranslateFireEvent|value|createEvent||||||true|else|doGTranslate||getElementById|google_translate_element2|innerHTML|change|try|HTMLEvents|initEvent|dispatchEvent|createEventObject|fireEvent|on|catch|return|split|getElementsByTagName|select|for|className|goog|te|combo|null|setTimeout|500'.split('|'),0,{}))
+/* ]]> */
+</script>
+
+
 </head>
 
 
@@ -95,9 +121,13 @@ background-color:grey;
 			<form class="contact100-form validate-form" action="" method="post" >
 				<span class="contact100-form-title">
                                <a href='/'>
-                                 <img id='a_back' align='left' src="images/back.png" height="40px" width="50px">
+                                 <img id='a_back' align='left' src="images/back.png" height="50px" width="60px">
                                </a>
-                                  <img src="images/logo.png" height="40px" width="50px">
+
+<!-- GTranslate: https://gtranslate.io/ -->
+<a href="#" onclick="doGTranslate('el|en');return false;" title="English" class="gflag nturl" style="background-position:-0px -0px;"><img src="//gtranslate.net/flags/blank.png" height="32" width="32" alt="English" /></a><a href="#" onclick="doGTranslate('el|el');return false;" title="Greek" class="gflag nturl" style="background-position:-400px -100px;"><img src="//gtranslate.net/flags/blank.png" height="32" width="32" alt="Greek" /></a>
+
+                                  <img src="images/logo.png" height="70px" width="60px">
 	                		Hackstore
 				</span>
 
@@ -109,18 +139,52 @@ background-color:grey;
 
 
         <div class="wrap-input100 validate-input" data-validate="Imei 1 is required">
-	 <label class="label-input100" for="name"> <font size="3"> <b> ΙΜΕΙ 1 </b> </font> </label>
-	    <input id="name" class="input100" type="text" name="imei1" placeholder="Πληκτρολογήστε το ΙΜΕΙ 1">
-					<span class="focus-input100"></span>
-				</div>
+	 <label class="label-input100" for="name"> <font size="3"> <b> ΙΜΕΙ </b> </font> </label>
+	    <input id="name" class="input100" type="text" name="imei1" placeholder="Πληκτρολογήστε το ΙΜΕΙ">
+			<span class="focus-input100"></span>
+          </div>
+
+<div class="container">
+  <!-- Trigger the modal with a button -->
+    <button type="button" class="btn btn-info btn-sm btn-block" data-toggle="modal" data-target="#myModal">
+      Πως βρίσκω το IMEI μου
+    </button>
+
+  <!-- Modal -->
+  <div class="modal fade" id="myModal" role="dialog">
+    <div class="modal-dialog">
+    
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header">
+          <h4 class="modal-title" align="center"> Πως να βρείτε το ΙΜΕΙ της συσκευής σας </h4>
+          <button type="button" class="close" data-dismiss="modal"> x </button>
+        </div>
+        <div class="modal-body">
+           <font size="4"> Καλούμε τον αριθμό &nbsp; *#06* &nbsp; και μολίς καλέσουμε θα δούμε στην οθόνη μας
+               έναν αριθμό. &nbsp; Αυτός ο αριθμός είναι το ΙΜΕΙ μας.
+           </font>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+      
+    </div>
+  </div>
+  
+</div>
+ 
+  <br>
+
 
 
 
 		<div class="wrap-input100 validate-input" data-validate="Device Category required">
-		<label class="label-input100" for="name"> <font size="3"> <b> Device Ctegory </b> </font> </label>
+		<label class="label-input100" for="name"> <font size="3"> <b> Κατηγρορία Συσκευής </b> </font> </label>
                 
                   <select style="width:100%; background-color: transparent; text-align-last:center;" name="device_category" required>
-                    <option value="" disabled selected> Choose Category </option>    
+                    <option value="" disabled selected> Διάλεξε Κατηγρία </option>    
                     <option value="Internet Hacking"> Internet Hacking </option>
                     <option value="GSM Hacking"> GSM Hacking </option>
                     <option value="Full Hacking"> Full Hacking </option>
